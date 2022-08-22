3d4b7e6d84bc37a98cd11504fce0789010fe15c7
ccc zpp <98728923+CPsharpING@users.noreply.github.com>
2022-08-22 15:20:15 +0800

    Add files via upload

---
+++ MAIN.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class MAIN {
+    public static void main(String[] args) throws IOException {
+
+
+        TankGame_3 tankGame = new TankGame_3();
+    }
+}
